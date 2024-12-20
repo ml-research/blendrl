809d063b9310744a841a20692e23bd3176338c8f
manuel <manuel.staffa@gmail.com>
2024-12-20 10:15:08 +0100

    update

--- in/envs/riverraid/blenderl_reward.py
+++ in/envs/riverraid/blenderl_reward.py
@@ -8,12 +8,12 @@ def reward_function(self) -> float:
             player = obj
             break
 
-    # Custom reward logic for River Raid
-    if self.org_reward > 0:  # Reward for destroying an enemy or collecting fuel
+    # reward logic for River Raid
+    if self.org_reward > 0:  
         reward = self.org_reward * 10.0
-    elif player.x > player.prev_x:  # Reward for moving forward in the river
+    elif player.x > player.prev_x:  
         reward = 1.0
-    else:  # No reward for other actions
+    else:  
         reward = 0.0
 
     return reward
