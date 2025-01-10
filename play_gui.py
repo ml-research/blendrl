532357ade0d71f2e6ff4ffcb6ca391fc4edb52e0
InnaK <inna.kuzmina@stud.tu-darmstadt.de>
2025-01-10 18:02:06 +0100

    fixed logic rules for pong

--- play_gui.py
+++ play_gui.py
@@ -26,8 +26,8 @@ def main(
         env_name=env_name,
         fps=fps,
         deterministic=False,
-        # env_kwargs=dict(render_oc_overlay=True),
-        env_kwargs=dict(render_oc_overlay=False),
+        env_kwargs=dict(render_oc_overlay=True),
+        #env_kwargs=dict(render_oc_overlay=False),
         render_predicate_probs=True,
         seed = seed)
     renderer.run()
