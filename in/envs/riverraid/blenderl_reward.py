e07b6bcd0e8ae8295d5bb2d1c8f0c10fdd6b8a23
manuel <manuel.staffa@gmail.com>
2025-01-30 00:10:25 +0100

    changed environment, rewards, updated packages

--- in/envs/riverraid/blenderl_reward.py
+++ in/envs/riverraid/blenderl_reward.py
@@ -3,17 +3,19 @@ import numpy as np
 
 def reward_function(self) -> float:
     # Find the player object
-    for obj in self.objects:
-        if "player" in str(obj).lower():
-            player = obj
-            break
+    player = next((obj for obj in self.objects if "player" in str(obj).lower()), None)
 
-    # reward logic for River Raid
-    if self.org_reward > 0:  
-        reward = self.org_reward * 10.0
-    elif player.x > player.prev_x:  
-        reward = 1.0
-    else:  
-        reward = 0.0
+    if player is None:
+        return 0.0  # No reward if player is not found
+
+    # Reward logic for River Raid
+    if self.org_reward > 0:
+        reward = (
+            self.org_reward * 10.0
+        )  # Higher reward for shooting enemies or collecting fuel
+    elif player.x > player.prev_x:
+        reward = 1.0  # Small reward for forward movement
+    else:
+        reward = -0.1  # Penalize stagnation or backward movement
 
     return reward
