7261b33998624cb40676d2ed326c0a0496f1f48e
Domi2208 <dominiceib@yahoo.de>
2025-02-23 23:49:41 +0100

    initial commit

---
+++ in/envs/spaceinvaders/valuation.py
@@ -0,0 +1,10 @@
+import torch as th
+from nsfr.utils.common import bool_to_probs
+
+""" in ocatari/ram/spaceinvaders.py :
+    MAX_NB_OBJECTS = {'Player': 1, 'Shield': 3,
+                  'Bullet': 3, 'Satellite': 1, 'Alien': 36}
+    
+"""
+
+
